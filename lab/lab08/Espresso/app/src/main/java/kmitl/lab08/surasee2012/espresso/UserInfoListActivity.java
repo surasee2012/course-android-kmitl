cf6562e04d549897f169d1903f4ed71d6c933b04
surasee2012 <30916904+surasee2012@users.noreply.github.com>
2017-10-26 17:40:41 +0700

    Submit homework Lab08
    - Update MainActivityTest, each test is independent.

--- lab/lab08/Espresso/app/src/main/java/kmitl/lab08/surasee2012/espresso/UserInfoListActivity.java
+++ lab/lab08/Espresso/app/src/main/java/kmitl/lab08/surasee2012/espresso/UserInfoListActivity.java
@@ -1,6 +1,5 @@
 package kmitl.lab08.surasee2012.espresso;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
